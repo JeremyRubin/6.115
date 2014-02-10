9f16787378ef13f2ffab67688eb0e8643784b2d6
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2014-02-10 17:05:15 -0500

    Create typewriter_newline.asm

---
+++ lab1/ex3/typewriter_newline.asm
@@ -0,0 +1,42 @@
+.org 00h             ; power up and reset vector
+    mov P1, #00h
+    ljmp start       ; when the micro wakes up, jump to the beginning of
+                     ; the main body or loop in the program called start
+.org 100h            ; and located at 100h in memory
+start:
+    lcall init       ; start the serial port by calling subroutine init
+    loop:            ; Now, endlessly repeat a loop that
+        lcall getchr ; <- gets a char from pc keyboard
+	mov P1, acc  ; Show the character on LED bank
+        lcall sndchr ; -> and echoes on the screen
+	sjmp clrf
+clrf:
+    djnz r1, loop
+    mov r1, #65
+    mov a, #10
+    lcall sndchr
+    mov a, #13
+    lcall sndchr
+    sjmp loop
+	
+init:
+    mov r1, #65
+    mov tmod, #20h  ; set timer 1 for auto reload - mode 2
+    mov tcon, #40h  ; run timer 1
+    mov th1,  #253        ; set 9600 baud with xtal=11.059mhz
+    mov scon, #54h  ; set serial control reg for 
+getchr:
+    jnb ri, getchr
+    mov a, sbuf
+    anl a, #7fh
+    clr ri
+    ret
+sndchr:
+    clr scon.1
+    mov sbuf,a
+txloop:
+    jnb scon.1, txloop
+    RET
+;Leave this here
+
+;If you want to be able to compile
