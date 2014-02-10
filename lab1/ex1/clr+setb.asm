791e6210e1533ebe5e1b6d3f1f0b92cc9ca8b5c3
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2014-02-10 13:13:01 -0500

    Update clr+setb.asm

--- lab1/ex1/clr+setb.asm
+++ lab1/ex1/clr+setb.asm
@@ -1,6 +1,10 @@
+;;; Set only the msb, set the lsb, then clear the msb
 .org 8000h
     mov P1, #80h
     setb P1.0
     clr P1.7
 loop:
     sjmp loop
+;Leave this here
+
+;If you want to be able to compile
