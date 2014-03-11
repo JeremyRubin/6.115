8a233660dda73c45efd843f65773721d86022e42
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2014-03-11 12:22:44 -0400

    removed hex, added printable

--- 3rdLab/3ex/sintblgen.py
+++ 3rdLab/3ex/sintblgen.py
@@ -1,4 +1,10 @@
 import numpy as np
-print ".db "+".db ".join([ "h, ".join(map(lambda x: '0'+hex(x)[2:-1],seg))+"h\n" for seg in np.split((np.sin(np.linspace(0,np.pi*2,257))[:-1]+1)*255//2, 32)])
-
-
+from matplotlib import pyplot as plt
+x = np.linspace(0, np.pi*2, 257)[:-1]
+nums = ((np.sin(x)+1)*255.0/2.0).astype(int)
+plt.plot(x,nums)
+plt.show()
+data = [ "h, ".join(map(lambda x: '0'+hex(x)[2:-1],seg))+"h\n" for seg in np.split(nums,32) ]
+plt.plot(x, [int("0x"+num.strip()[1:-1],16) for num in (",".join(data)).split(",")])
+plt.show()
+print ".db "+".db ".join(data)
